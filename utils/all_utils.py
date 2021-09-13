26cf183932f4975f44f5080f63ef6c2e2b24a9dd
Praveenku32k <68581081+Praveenku32k@users.noreply.github.com>
2021-09-13 08:22:19 +0530

    First commit

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -6,31 +6,32 @@ from matplotlib.colors import ListedColormap
 import os 
 
 
+
 plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
 
 
 def prepare_data(df):
-      X = df.drop("y", axis=1)
-
-  y = df["y"]
-
-  return X, y
-
+     X = df.drop("y", axis=1)
+     y = df["y"]
+     return X, y
+  
 def save_model(model, filename):
-      model_dir = "models"
-  os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  filePath = os.path.join(model_dir, filename) # model/filename
-  joblib.dump(model, filePath) 
+      
+    model_dir = "models"
+    os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
+    filePath = os.path.join(model_dir, filename) # model/filename
+    joblib.dump(model, filePath) 
 
 def save_plot(df, file_name, model):
-      def _create_base_plot(df):
-    df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
-    plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
-    plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
-    figure = plt.gcf() # get current figure
-    figure.set_size_inches(10, 8)
+      
+    def _create_base_plot(df):
+      df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
+      plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
+      plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
+      figure = plt.gcf() # get current figure
+      figure.set_size_inches(10, 8)
 
-  def _plot_decision_regions(X, y, classfier, resolution=0.02):
+def plot_decision_regions(X, y, classfier, resolution=0.02):
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
     cmap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -41,7 +42,7 @@ def save_plot(df, file_name, model):
     x2_min, x2_max = x2.min() -1 , x2.max() + 1  
 
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
-                           np.arange(x2_min, x2_max, resolution))
+                            np.arange(x2_min, x2_max, resolution))
     print(xx1)
     print(xx1.ravel())
     Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
@@ -54,13 +55,13 @@ def save_plot(df, file_name, model):
 
 
 
-  X, y = prepare_data(df)
+    X, y = prepare_data(df)
 
-  _create_base_plot(df)
-  _plot_decision_regions(X, y, model)
+    create_base_plot(df)
+    plot_decision_regions(X, y, model)
 
-  plot_dir = "plots"
-  os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  plotPath = os.path.join(plot_dir, file_name) # model/filename
-  plt.savefig(plotPath)
-  
+    plot_dir = "plots"
+    os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
+    plotPath = os.path.join(plot_dir, file_name) # model/filename
+    plt.savefig(plotPath)
+    
