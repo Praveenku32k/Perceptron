632e5f59c4b5f76fe2be6e5484bd9526052bf392
Praveenku32k <68581081+Praveenku32k@users.noreply.github.com>
2021-09-19 07:24:56 +0530

    docstring updated

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -8,17 +8,38 @@ import os
 plt.style.use("fivethirtyeight")
 
 def prepare_data(df):
+  """It is used to separate the dependent variable and independent variable  features
+
+  Args:
+      df (pd.DataFrame):It is pandas DataFrame to be
+
+  Returns:
+      tuple : It return the tuples of the dependent variable and independent variable
+  """
   X = df.drop("y", axis=1)
   y = df["y"]
   return X, y
 
 def save_model(model, filename):
+  """This saves the train model to    
+
+  Args:
+      model (Python object): trained model to
+  Returns:
+      filename (str): path to save the  trained model
+  """
   model_dir = "models"
   os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   filePath = os.path.join(model_dir, filename) # model/filename
   joblib.dump(model, filePath)
 
 def save_plot(df, file_name, model):
+      """
+      :param df: Its a DataFrame
+      : param filename: its paths to save the plot and
+      :param model:trained model
+      """
+      
   def _create_base_plot(df):
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
