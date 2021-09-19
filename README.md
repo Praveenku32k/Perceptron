11e7d9c4cec7f5127ecd83ea15125c38976598a8
Praveenku32k <68581081+Praveenku32k@users.noreply.github.com>
2021-09-19 07:34:10 +0530

    docstring updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Perceptron
-Ann first work
+ANN Perceptron first project
+
+
+# Commands used
+ 
+```bash
+git add . && git commit -m "docstring updated" && git push origin main
+
+```
--- or.py
+++ or.py
@@ -1,3 +1,9 @@
+"""
+author: Praveen kumar
+email:praveenku32k@gmail.com
+phone Number: 9006336740
+"""
+
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
